7cd69bff2668823f86a59221685ebfab5559e988
Dimas Abidzar Fadly <dimasfadly01@gmail.com>
2025-12-22 13:42:27 +0700

    rollback script url

--- client/tambahspk/script.js
+++ client/tambahspk/script.js
@@ -1,5 +1,6 @@
 const API_PROXY_URL = "https://sparta-backend-5hdj.onrender.com/api/form";
-const GAS_LOGIN_VERIFY_URL = "https://sparta-backend-5hdj.onrender.com/api/form?form=login_perpanjanganspk&action=verifyToken";
+const GAS_LOGIN_VERIFY_URL = "https://sparta-backend-m3ms.onrender.com/api/form?form=login_perpanjanganspk&action=verifyToken";
+const LOGIN_PAGE_URL = "https://frontend-form-virid.vercel.app/login-perpanjanganspk.html";
 
 let currentUser = null;
 let ulokData = []; 
